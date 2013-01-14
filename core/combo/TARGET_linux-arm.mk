be5c2c85397f9e0c36f638fa082a387b8f414cc6
loosethisskin <loosethisskin@gmail.com>
2013-01-13 19:19:14 -0500

    Option to use Linaro's compiler flags (see 9c83953)
    To enable: export USE_LINARO_COMPILER_FLAGS=yes
    
    Set LD flags to something safe by default.
    
    Original commit: https://github.com/pbeeler/android_build/commit/f54beb181365ac0a1659e9404670789c3d81e578

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -68,18 +68,34 @@ endif
 
 TARGET_NO_UNDEFINED_LDFLAGS := -Wl,--no-undefined
 
-TARGET_arm_CFLAGS :=    -O2 \
-                        -fomit-frame-pointer \
-                        -fstrict-aliasing    \
-                        -funswitch-loops
+ifeq ($(USE_LINARO_COMPILER_FLAGS),yes)
+    TARGET_arm_CFLAGS :=    -O3 \
+                            -fomit-frame-pointer \
+                            -fstrict-aliasing    \
+                            -funswitch-loops
+else
+    TARGET_arm_CFLAGS :=    -O2 \
+                            -fomit-frame-pointer \
+                            -fstrict-aliasing    \
+                            -funswitch-loops
+endif 
 
 # Modules can choose to compile some source as thumb.
-TARGET_thumb_CFLAGS :=  -mthumb \
-                        -Os \
-                        -fomit-frame-pointer \
-                        -fstrict-aliasing \
-                        -Wstrict-aliasing=2 \
-                        -Werror=strict-aliasing
+ifeq ($(USE_LINARO_COMPILER_FLAGS),yes)
+        TARGET_thumb_CFLAGS :=  -mthumb \
+                                -O3 \
+                                -fomit-frame-pointer \
+                                -fstrict-aliasing \
+                                -Wstrict-aliasing=2 \
+                                -Werror=strict-aliasing
+else
+        TARGET_thumb_CFLAGS :=  -mthumb \
+                                -Os \
+                                -fomit-frame-pointer \
+                                -fstrict-aliasing \
+                                -Wstrict-aliasing=2 \
+                                -Werror=strict-aliasing
+endif
 
 # Turn off strict-aliasing if we're building an AOSP variant without the
 # patchset...
